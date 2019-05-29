dc933d4fc255197d5912107f03ca001b05f555e4
zy1995625s <zy1995625s@gmail.com>
2019-05-29 22:01:41 +0800

    LCD灯

--- main/main.js
+++ main/main.js
@@ -1,4 +1,30 @@
-module.exports = function main() {
-    console.log("Debug Info");
-    return 'Hello World!';
+const lcdNum = [
+    ["._.", "|.|", "|_|"],
+    ["...", "..|", "..|"],
+    ["._.", "._|", "|_."],
+    ["._.", "._|", "._|"],
+    ["...", "|_|", "..|"],
+    ["._.", "|_.", "._|"],
+    ["._.", "|_.", "|_|"],
+    ["._.", "..|", "..|"],
+    ["._.", "|_|", "|_|"],
+    ["._.", "|_|", "..|"]
+];
+
+module.exports = function main(digit) {
+    var ans = new String();
+
+    for(var i = 0; i<3; i++) {
+        for(var j = 0; j<digit.length; j++) {
+            var num = parseInt(digit.charAt(j));
+            ans = ans.concat(lcdNum[num][i]);
+            if(j < digit.length -1) {
+                ans = ans.concat(" ");
+            } else {
+                ans = ans.concat("\n");
+            }
+        }
+    }
+
+    return ans;
 };
